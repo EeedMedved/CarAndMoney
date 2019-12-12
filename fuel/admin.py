d04d2dce6ead78115cd5f0e0f349fdbab90d04cc
eeedmedved <github.com@sennator.ru>
2019-12-12 18:21:32 +0300

    Добавил модели Refill (заправка бензином) и GasStation (АЗС)

--- fuel/admin.py
+++ fuel/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import GasStation
+
+admin.site.register(GasStation)
--- fuel/models.py
+++ fuel/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Refill(models.Model):
+    refillDate = models.DateField()
+    mileage = models.IntegerField(unique=True)
+    liters = models.SmallIntegerField()
+    gas_station = models.ForeignKey('GasStation', on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    comment = models.CharField(max_length=100, blank=True, null=True)
+
+    @property
+    def sum(self):
+        return self.liters * self.price
+
+
+class GasStation(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+
+    class Meta:
+        unique_together = ['name', 'location']
